6775e197b9225c2086f86eb12f72dab0cb1dbb73
Shreya Saxena <shreya.saxena@gmail.com>
2024-11-30 14:43:38 -0500

    Added descriptions for all projects.

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -70,7 +70,7 @@ function Projects() {
 
           {/* Text and Details Section */}
           <div className="ml-4 flex-grow">
-            <h2 className="text-2xl font-bold mb-2">AI Chatbot Project</h2>
+            <h2 className="text-2xl font-bold mb-2">Chatbot Generation Guide</h2>
             <p className="text-sm md:text-base lg:text-lg mb-2">
               A modular AI chatbot built using AIML and Java to enhance student
               learning in artificial intelligence. The project emphasizes
@@ -116,6 +116,21 @@ function Projects() {
               Your browser does not support SVG.
             </object>
           </a>
+           {/* Text and Details Section */}
+           <div className="ml-4 flex-grow">
+            <h2 className="text-2xl font-bold mb-2">Pantry Sidekick</h2>
+            <p className="text-sm md:text-base lg:text-lg mb-2">
+            A ML-powered food management app to streamline pantry inventory tracking and reduce food waste, created in over 40 hours for the Girls in Tech Hackathon 2024.
+            </p>
+
+            <h3 className="text-md font-semibold mb-1">Key Features:</h3>
+            <ul className="list-disc list-inside text-sm md:text-base lg:text-lg">
+              <li>Real-time pantry item recognition using YOLOv8 with CUDA and a custom Roboflow-trained dataset.</li>
+              <li>Flask-based backend with REST API and responsive HTML/CSS for seamless inventory management.</li>
+              <li>ML-powered spoilage prediction and recipe suggestions to minimize food waste.</li>
+            </ul>
+            <h3 className="text-md font-semibold mb-1">Tech Stack:</h3>
+          </div>
         </div>
 
         {/* Section 3 */}
@@ -146,6 +161,20 @@ function Projects() {
               Your browser does not support SVG.
             </object>
           </a>
+          {/* Text and Details Section */}
+          <div className="ml-4 flex-grow">
+            <h2 className="text-2xl font-bold mb-2">Aid-Finder</h2>
+            <p className="text-sm md:text-base lg:text-lg mb-2">
+            AidFinder is an Android application designed to assist homeless individuals and those facing food insecurity in locating nearby shelters and food banks, featuring map navigation and a review system for real-time, user-friendly support.            </p>
+
+            <h3 className="text-md font-semibold mb-1">Key Features:</h3>
+            <ul className="list-disc list-inside text-sm md:text-base lg:text-lg">
+              <li>Real-time geolocation with GPS APIs and map frameworks for dynamic routing to shelters/food banks.</li>
+              <li>Google Cloud Services for secure storage and real-time synchronization of user reviews and ratings.</li>
+              <li>WCAG-compliant UI with adaptive layouts and Google Cloud Translation API for multilingual support.</li>
+            </ul>
+            <h3 className="text-md font-semibold mb-1">Tech Stack:</h3>
+          </div>
         </div>
 
         {/* Section 4 */}
@@ -176,6 +205,19 @@ function Projects() {
               Your browser does not support SVG.
             </object>
           </a>
+
+          <div className="ml-4 flex-grow">
+            <h2 className="text-2xl font-bold mb-2">The Gandhian Code</h2>
+            <p className="text-sm md:text-base lg:text-lg mb-2">
+            Created for the Adobe Creative Challenge, celebrating 150 years of Gandhian virtues with a focus on non-violence and its relevance in modern society.</p>
+            <h3 className="text-md font-semibold mb-1">Key Features:</h3>
+            <ul className="list-disc list-inside text-sm md:text-base lg:text-lg">
+              <li>Built with Adobe XD featuring scroll-based interactions, smooth transitions, and navigable panels.</li>
+              <li>Detailed illustrations and optimized visuals using Adobe Photoshop and Adobe Stock.</li>
+              <li>Modular layout showcasing Gandhian movements, ideologies, and policies for easy navigation and engagement.</li>
+            </ul>
+            <h3 className="text-md font-semibold mb-1">Tech Stack:</h3>
+          </div>
         </div>
 
         {/* Section 5 */}
@@ -206,6 +248,18 @@ function Projects() {
               Your browser does not support SVG.
             </object>
           </a>
+          <div className="ml-4 flex-grow">
+            <h2 className="text-2xl font-bold mb-2">Aircraft Analysis</h2>
+            <p className="text-sm md:text-base lg:text-lg mb-2">
+            A comprehensive data analysis project analyzing 100 years of aircraft incidents [1920-2020] using Python libraries uncovering trends in aviation safety, fatalities, and factors through interactive visualizations.</p>
+            <h3 className="text-md font-semibold mb-1">Key Features:</h3>
+            <ul className="list-disc list-inside text-sm md:text-base lg:text-lg">
+              <li>Extracted incident data using BeautifulSoup for web scraping.</li>
+              <li>Analyzed trends with pandas and numpy, visualized using matplotlib, seaborn, and plotly.</li>
+              <li>Interactive plots to explore relationships between aircraft types, timing, and operator locations.</li>
+            </ul>
+            <h3 className="text-md font-semibold mb-1">Tech Stack:</h3>
+          </div>
         </div>
 
         {/* Section 6 */}
@@ -236,6 +290,18 @@ function Projects() {
               Your browser does not support SVG.
             </object>
           </a>
+          <div className="ml-4 flex-grow">
+            <h2 className="text-2xl font-bold mb-2">Carbon Emission Calculator</h2>
+            <p className="text-sm md:text-base lg:text-lg mb-2">
+            A Carbon Emission Calculator website to assess the carbon footprint of laptops by automating battery report data collection, processing CO2 emissions, and providing user-friendly insights.</p>
+            <h3 className="text-md font-semibold mb-1">Key Features:</h3>
+            <ul className="list-disc list-inside text-sm md:text-base lg:text-lg">
+              <li>Backend in Django with APIs to calculate CO2 emissions from battery report data.</li>
+              <li>Automated battery report data collection and processing, storing results in a Postgres database.</li>
+              <li>Web interface with Bootstrap for viewing power consumption and tracking emissions.</li>
+            </ul>
+            <h3 className="text-md font-semibold mb-1">Tech Stack:</h3>
+          </div>
         </div>
 
         {/* Section 7 */}
@@ -266,6 +332,18 @@ function Projects() {
               Your browser does not support SVG.
             </object>
           </a>
+          <div className="ml-4 flex-grow">
+            <h2 className="text-2xl font-bold mb-2">Ai driven wildfire research</h2>
+            <p className="text-sm md:text-base lg:text-lg mb-2">
+            An AI-driven wildfire risk prediction model on Microsoft Azure, utilizing IoT and satellite data for real-time analytics, paired with a user-friendly web interface for live risk data visualization.</p>
+            <h3 className="text-md font-semibold mb-1">Key Features:</h3>
+            <ul className="list-disc list-inside text-sm md:text-base lg:text-lg">
+              <li>IoT and satellite remote sensing data utilised via Microsoft Azure for real-time wildfire risk prediction.</li>
+              <li>Backend system with optimized data processing and DBMS for seamless integration with AI models.</li>
+              <li>Web interface for live risk visualization and predictive analytics to support wildfire management.</li>
+            </ul>
+            <h3 className="text-md font-semibold mb-1">Tech Stack:</h3>
+          </div>
         </div>
       </div>
     </div>
