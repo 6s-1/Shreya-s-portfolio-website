f065b994fd3cfbb40a69ac66365eaadefcc0c7de
Shreya Saxena <shreya.saxena@gmail.com>
2024-12-01 16:37:51 -0500

    updated appRouter

--- src/appRouter.jsx
+++ src/appRouter.jsx
@@ -1,15 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './App'; // Main page
 import ProjectsPage from './ProjectsPage'; // Projects page
 
 function AppRouter() {
   return (
+    <BrowserRouter
+      basename={import.meta.env.DEV ? '/' : '/react-vite-gh-pages/'}
+    >
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<ProjectsPage />} />
       </Routes>
     </Router>
+     </BrowserRouter>
   );
 }
 
