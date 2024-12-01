33cf20e4f5719c998ff9bab39556976d8970d5e5
Shreya Saxena <shreya.saxena@gmail.com>
2024-12-01 16:39:50 -0500

    updated appRouter

--- src/appRouter.jsx
+++ src/appRouter.jsx
@@ -5,7 +5,7 @@ import ProjectsPage from './ProjectsPage'; // Projects page
 function AppRouter() {
   return (
     <BrowserRouter
-      basename={import.meta.env.DEV ? '/' : '/react-vite-gh-pages/'}
+      basename={import.meta.env.DEV ? '/Shreya-s-personal-portfolio/' : '/react-vite-gh-pages/'}
     >
     <Router>
       <Routes>
