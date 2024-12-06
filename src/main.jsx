0abdac6074d8cced28632e0d03928814866c4d47
Shreya Saxena <shreya.saxena@gmail.com>
2024-12-06 03:42:02 -0500

    Updated files, added Background SVGs

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router /*basename="/Shreya-s-portfolio-website"*/> {/* Replace <repository-name> with your actual repository name */}
+    <Router basename="/Shreya-s-portfolio-website"> {/* Replace <repository-name> with your actual repository name */}
       <App />
     </Router>
   </StrictMode>,
